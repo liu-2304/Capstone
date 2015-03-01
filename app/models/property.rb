f34db59947866c090a7d983d4c57b9f4d318cb89
Adam Mckenzie <mckenzie.101@server3.ies.cse.ohio-state.edu>
2015-03-01 16:41:03 -0500

    Update: filterrific new issues

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,2 +1,8 @@
 class Property < ActiveRecord::Base
+  belongs_to :material
+  
+  attr_accessible :density, :elastic_modulus, :shear_modulus, :poissons_strength, :yield_strength,
+:ultimate_tensile_strength, :ultimate_total_elongation, :hardness_value, :melting_point, 
+:thermal_conductivity, :specific_heat, :electrical_resistivity, :chemical_composition
+
 end
